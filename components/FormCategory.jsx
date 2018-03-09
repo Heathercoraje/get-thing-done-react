b1c13c0e221b96a4ae73f92106557515a1de1583
heathercoraje <yhk7830@hotmail.com>
2018-03-09 16:57:23 +0900

    toggle input this does not have add function attached yet

--- components/FormCategory.jsx
+++ components/FormCategory.jsx
@@ -4,22 +4,34 @@ class FormCategory extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputOpen: true
+      inputOpen: false
     };
     this.onClickInput = this.onClickInput.bind(this);
   }
 
+  // componentDidMount() {
+  //   const target = document.getElementById('input');
+  //   if (target.style.color == red) {
+  //     target.style.color == blue;
+  //   }
+  // }
   onClickInput(event) {
     event.preventDefault();
-    const name = event.target[0].value;
-    this.props.add(name);
-    event.target[0].value = '';
+    const input = document.getElementById('input');
+    if (input.style.display === 'none') {
+      input.style.display = 'block';
+    } else {
+      input.style.display = 'none';
+    }
+    // const name = event.target[0].value;
+    // this.props.add(name);
+    // event.target[0].value = '';
   }
 
   render() {
     return (
       <form onSubmit={this.onClickInput}>
-        <input required placeholder="new category" />
+        <input placeholder="new category" id="input" style={{ display: 'none' }} />
         <input type="submit" value="+" className="button-add" />
       </form>
     );
