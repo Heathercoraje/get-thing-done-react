22b845f1290bbd4ba558ab70ad78ab740a2f97f9
heathercoraje <yhk7830@hotmail.com>
2018-03-09 17:29:47 +0900

    add function attached also handles 3 different user cases

--- components/FormCategory.jsx
+++ components/FormCategory.jsx
@@ -3,29 +3,25 @@ import React, { Component } from 'react';
 class FormCategory extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputOpen: false
-    };
+    this.state = {};
     this.onClickInput = this.onClickInput.bind(this);
   }
 
-  // componentDidMount() {
-  //   const target = document.getElementById('input');
-  //   if (target.style.color == red) {
-  //     target.style.color == blue;
-  //   }
-  // }
   onClickInput(event) {
     event.preventDefault();
     const input = document.getElementById('input');
     if (input.style.display === 'none') {
       input.style.display = 'block';
+      input.focus();
+    } else if (input.style.display === 'block' && event.target[0].value.trim() !== '') {
+      const name = event.target[0].value;
+      this.props.add(name);
+      event.target[0].value = '';
+      input.style.display = 'none';
     } else {
+      // input field is empty or white space
       input.style.display = 'none';
     }
-    // const name = event.target[0].value;
-    // this.props.add(name);
-    // event.target[0].value = '';
   }
 
   render() {
