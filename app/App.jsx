363b078a85642832b3312458081548944b87184a
heathercoraje <yhk7830@hotmail.com>
2018-03-18 02:48:00 +0900

    refactor refactor adding spread operator for props, fix index problem with complete function

--- app/App.jsx
+++ app/App.jsx
@@ -12,82 +12,93 @@ const Wrapper = styled.div`
 `;
 class App extends Component {
 	state = {
-	  categories: [{ id: 0, name: 'Todo', desc: 'Get things done today' }],
-	  selectedCategory: 0
+		categories: [
+			{ id: 0, name: 'Todo', description: 'Add description of category' }
+		],
+		selectedCategory: 0
 	};
 
-	addCategory = (event) => {
-	  const Categories = this.state.categories;
-	  const Todos = this.state.todos;
-	  const Name = event.target[0].value.charAt(0).toUpperCase() + event.target[0].value.slice(1);
-	  if (CheckDupli(Name, Categories)) {
-	    alert('duplicate cannot be added!');
-	    return;
-	  }
-	  const newCategory = {
-	    id: Categories.length ? Categories[Categories.length - 1].id + 1 : 0,
-	    name: Name,
-	    desc: ''
-	  };
-	  Categories.push(newCategory);
-	  const toDisplay = Categories.length - 1;
-	  this.setState({
-	    categories: Categories,
-	    selectedCategory: toDisplay,
-	    todos: Todos
-	  });
+	addCategory = event => {
+		const Categories = this.state.categories;
+		const Todos = this.state.todos;
+		const Name =
+			event.target[0].value.charAt(0).toUpperCase() +
+			event.target[0].value.slice(1);
+		if (CheckDupli(Name, Categories)) {
+			alert('duplicate cannot be added!');
+			return;
+		}
+		const newCategory = {
+			id: Categories.length ? Categories[Categories.length - 1].id + 1 : 0,
+			name: Name,
+			description: 'New category'
+		};
+		Categories.push(newCategory);
+		const toDisplay = Categories.length - 1;
+		this.setState({
+			categories: Categories,
+			selectedCategory: toDisplay,
+			todos: Todos
+		});
 	};
-	deleteCategory = (event) => {
-	  console.log('?');
-	  const Categories = this.state.categories;
-	  const PrevToDisplay = this.state.selectedCategory;
-	  const Todos = this.state.todos;
-	  const newCategories = Categories.filter(category => category.id !== Number(event.target.value));
-	  const toDisplay =
-			Number(event.target.value) === PrevToDisplay ? PrevToDisplay - 1 : newCategories.length - 1;
-	  this.setState({
-	    categories: newCategories,
-	    selectedCategory: toDisplay
-	  });
+	deleteCategory = event => {
+		console.log('?');
+		const Categories = this.state.categories;
+		const PrevToDisplay = this.state.selectedCategory;
+		const Todos = this.state.todos;
+		const newCategories = Categories.filter(
+			category => category.id !== Number(event.target.value)
+		);
+		const toDisplay =
+			Number(event.target.value) === PrevToDisplay
+				? PrevToDisplay - 1
+				: newCategories.length - 1;
+		this.setState({
+			categories: newCategories,
+			selectedCategory: toDisplay
+		});
 	};
-	HandleDesc = (evt) => {
-	  evt.preventDefault();
-	  const current = this.state.categories;
-	  const selected = this.state.selectedCategory;
-	  const targetCate = current[selected];
-	  delete targetCate.desc;
-	  targetCate.desc = evt.target[0].value;
-	  console.log(targetCate.desc);
-	  evt.target[0].value = '';
-	  current.splice(selected, 1, targetCate);
-	  this.setState({
-	    categories: current
-	  });
+	HandleDesc = evt => {
+		evt.preventDefault();
+		const { categories, selectedCategory } = this.state;
+		const updatedCategory = {
+			...categories[selectedCategory],
+			description: !evt.target[0] ? evt.target.value : evt.target[0].value
+		};
+
+		evt.target.value = '';
+		this.setState({
+			categories: [
+				...categories.slice(0, selectedCategory),
+				updatedCategory,
+				...categories.slice(selectedCategory + 1)
+			]
+		});
 	};
-	clickCate = (evt) => {
-	  const toDisplay = evt.target.value;
-	  this.setState({
-	    selectedCategory: toDisplay
-	  });
+	clickCate = evt => {
+		const toDisplay = evt.target.value;
+		this.setState({
+			selectedCategory: toDisplay
+		});
 	};
 
 	render() {
-	  return (
-	    <Wrapper>
-	      <Category
-	        click={this.clickCate}
-	        categories={this.state.categories}
-	        selected={this.state.selectedCategory}
-	        delete={this.deleteCategory}
-	        add={this.addCategory}
-	      />
-	      <Todo
-	        categories={this.state.categories}
-	        selected={this.state.selectedCategory}
-	        HandleDesc={this.HandleDesc}
-	      />
-	    </Wrapper>
-	  );
+		return (
+			<Wrapper>
+				<Category
+					click={this.clickCate}
+					categories={this.state.categories}
+					selected={this.state.selectedCategory}
+					delete={this.deleteCategory}
+					add={this.addCategory}
+				/>
+				<Todo
+					categories={this.state.categories}
+					selected={this.state.selectedCategory}
+					HandleDesc={this.HandleDesc}
+				/>
+			</Wrapper>
+		);
 	}
 }
 
--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -5,52 +5,49 @@ import Desc from './Desc';
 const Wrapper = styled.div`
 	width: 100%;
 	overflow-y: auto;
+	overflow-x: hidden;
 `;
 
-const TodoList = (props) => {
-  const todos = props.todos;
-  const selected = props.selected;
-  const name = props.name;
-  const descText = props.categories[selected].desc;
-  const Renderlist = !todos
-    ? ''
-    : todos.map(todo => (
-      <TodoItem
-        todo={todo.todo}
-        key={todo.id}
-        id={todo.id}
-        isDone={todo.isDone}
-        value={todo.id}
-        delete={props.delete}
-        complete={props.complete}
-      />
-    ));
-  return (
-    <Wrapper>
-      <Desc
-        name={name}
-        desc={descText === '' ? 'Add description of category' : descText}
-        HandleDesc={props.HandleDesc}
-      />
-      <div>{Renderlist}</div>
-    </Wrapper>
-  );
+const TodoList = props => {
+	const todos = props.todos;
+	const selected = props.selected;
+	const name = props.name;
+	const descText = props.categories[selected].description;
+	const Renderlist = !todos
+		? ''
+		: todos.map(todo => (
+				<TodoItem
+					todo={todo.todo}
+					key={todo.id}
+					id={todo.id}
+					isDone={todo.isDone}
+					value={todo.id}
+					delete={props.delete}
+					complete={props.complete}
+				/>
+			));
+	return (
+		<Wrapper>
+			<Desc name={name} description={descText} HandleDesc={props.HandleDesc} />
+			<div>{Renderlist}</div>
+		</Wrapper>
+	);
 };
 
 const TodoItem = props => (
-  <div key={props.id} className="list-todo">
-    <button className="checkbox" onClick={props.delete} value={props.value}>
+	<div key={props.id} className="list-todo">
+		<button className="checkbox" onClick={props.delete} value={props.value}>
 			&#9744;
-    </button>
-    <li
-      id={props.id}
-      onClick={props.complete}
-      value={props.value}
-      style={!props.isDone ? null : { textDecoration: 'line-through' }}
-    >
-      {props.todo}
-    </li>
-  </div>
+		</button>
+		<li
+			id={props.id}
+			onClick={props.complete}
+			value={props.value}
+			style={!props.isDone ? null : { textDecoration: 'line-through' }}
+		>
+			{props.todo}
+		</li>
+	</div>
 );
 
 export default TodoList;
